8f6be38c471e2383c140a0d47432c4497fe62ece
Oliver Conover <fuchsfarbeart@outlook.com>
2023-12-23 20:56:22 -0500

    Refactor login and registration functionalities
    Removed methods from Login and Registration pages and moved the logic to corresponding form components to simplify the code structure. Additionally updated imports where necessary and adjusted namespace providers in EpochApp Client DotSettings.

--- EpochApp/Client/Pages/Auth/Registration.razor.cs
+++ EpochApp/Client/Pages/Auth/Registration.razor.cs
@@ -4,43 +4,9 @@
 // Oliver MacDougall
 // Modified: 29-11-2023
 
-using EpochApp.Client.Services;
-using EpochApp.Shared;
-using Microsoft.AspNetCore.Components;
-using System.Net.Http.Json;
-
 namespace EpochApp.Client.Pages.Auth
 {
     public partial class Registration
     {
-
-        private RegistrationDTO _registration = new RegistrationDTO();
-        [Inject] public HttpClient Http { get; set; }
-        [Inject] public NavigationManager NavigationManager { get; set; }
-        [Inject] public ILogger<Registration> Logger { get; set; }
-        [Inject] public EpochAuthProvider Auth { get; set; }
-
-        private async Task AttemptRegistrationAsync()
-        {
-            Logger.LogInformation("Attempting to register user: \n\t",
-                                  _registration.UserName, "\n\t\t",
-                                  _registration.Password, "\n\t\t",
-                                  _registration.DateOfBirth, "\n\t\t"
-                                  , _registration.Email, "\n\t\t");
-            var result = await Http.PostAsJsonAsync("api/v1/EpochUsers/Registration", _registration);
-            if (result.IsSuccessStatusCode)
-            {
-                await Auth.LoginAsync(_registration.UserName, _registration.Password);
-                NavigationManager.NavigateTo("/");
-            }
-            else
-            {
-                var errors = await result.Content.ReadFromJsonAsync<Dictionary<string, List<string>>>();
-                foreach (var error in errors)
-                {
-                    Logger.LogError($"Error registering user: {error.Key}\n\t", error.Value, "\n");
-                }
-            }
-        }
     }
 }
