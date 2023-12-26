8cbb75131ba2be34c8a46a2041eb140b343c3e7b
Oliver Conover <fuchsfarbeart@outlook.com>
2023-12-26 17:41:18 -0500

    Add form validation and user experience enhancements
    Improved form validation and user interface by adding support for user feedback on input fields. Added new classes to indicate the presence of validation errors and implemented an inline form option. Created new form field components including numeric input, date input, textarea, switch input, and select input to provide a more diverse and flexible range of input field options.

--- EpochApp/Client/Pages/Index.razor.cs
+++ EpochApp/Client/Pages/Index.razor.cs
@@ -6,13 +6,29 @@
 
 using EpochApp.Client.Services;
 using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Forms;
+using System.ComponentModel.DataAnnotations;
 
 namespace EpochApp.Client.Pages
 {
 
+    public class TestModel
+    {
+        [Required(ErrorMessage = "This field is required")]
+        public string? TextData { get; set; }
+        public int IntData { get; set; } = 0;
+        public float FloatData { get; set; } = 0.0f;
+        public double DoubleData { get; set; } = 0.0;
+        public decimal DecimalData { get; set; } = 0.0m;
+        public bool BoolData { get; set; } = true;
+        public DateTime DateData { get; set; } = DateTime.Now;
+        public string SelectData { get; set; }
+    }
+
     public partial class Index
     {
         private DataStore _dataStore = new DataStore();
+        private TestModel _testModel = new TestModel();
         [Inject] public ILocalStorage Storage { get; set; }
         public string StoredValue { get; set; } = "";
         public int TabIndex { get; set; }
@@ -36,6 +52,22 @@ namespace EpochApp.Client.Pages
         {
             await Storage.Clear();
             StoredValue = "";
+        }
+
+        private Task RefreshData(EditContext editContext)
+        {
+            if (!editContext.Validate())
+            {
+                Console.WriteLine("Invalid");
+            }
+            else
+            {
+                Console.WriteLine("Validated");
+                var mod = (TestModel)editContext.Model;
+                Console.WriteLine(mod.TextData);
+            }
+            StateHasChanged();
+            return Task.CompletedTask;
         }
 
         private class DataStore
