8c6c2e694de7e968e7ad1625739df725fd6f0a18
Oliver Conover <fuchsfarbeart@outlook.com>
2024-01-30 13:35:52 -0500

    Expand WorldDTO and update project
    Extended the WorldDTO with additional properties detailing more aspects of a world like names, dates, current date in the world, and metadata. Meanwhile, the EpochApp.Mobile project has been added to the solution. Also, some description comments were added for better code comprehension and FlatSwitchInput.razor was removed from Flatify project.

--- EpochApp/Client/Shared/Forms/WorldForm.razor.cs
+++ EpochApp/Client/Shared/Forms/WorldForm.razor.cs
@@ -5,9 +5,15 @@ using Microsoft.AspNetCore.Components.Forms;
 
 namespace EpochApp.Client.Shared.Forms
 {
+    /// <summary>
+    ///     The world form component.
+    /// </summary>
     public partial class WorldForm
     {
+        /// <summary> The user data. </summary>
         [Parameter] public UserData UserData { get; set; }
+
+        /// <summary> The world. </summary>
         [Parameter] public World World { get; set; } = new World();
         [Inject] public HttpClient Client { get; set; }
         [Inject] public NavigationManager Nav { get; set; }
