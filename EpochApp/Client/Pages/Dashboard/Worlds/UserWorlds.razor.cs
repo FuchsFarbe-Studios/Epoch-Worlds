725a2fa26f3852894a9e6cc244449baae43a50f8
Oliver Conover <fuchsfarbeart@outlook.com>
2024-02-27 06:06:44 -0500

    Update user's Worlds page design and logic
    The revamped UserWorlds page now uses MudCard for better information structuring and aesthetics. In addition, the page now fetches UserWorldDTO objects rather than WorldDTO objects to manage and display user's worlds. A deletion handler is added for the UserWorldDTO object and can prevent users from deleting their only world. This update improves the overall user-interaction experience on the page.

--- EpochApp/Client/Pages/Dashboard/Worlds/UserWorlds.razor.cs
+++ EpochApp/Client/Pages/Dashboard/Worlds/UserWorlds.razor.cs
@@ -6,12 +6,19 @@ using System.Net.Http.Json;
 
 namespace EpochApp.Client.Pages.Dashboard.Worlds
 {
+    /// <summary>
+    ///     Displays a list of the user's worlds.
+    /// </summary>
     public partial class UserWorlds
     {
+        private List<UserWorldDTO> _newUserWorlds = new List<UserWorldDTO>();
+        private List<WorldDTO> _userWorlds = new List<WorldDTO>();
+
         /// <summary> The active world. </summary>
         [CascadingParameter] protected WorldDTO ActiveWorld { get; set; }
 
-        private List<WorldDTO> _userWorlds = new List<WorldDTO>();
+        /// <summary> The new active world. </summary>
+        [CascadingParameter] protected UserWorldDTO NewActiveWorld { get; set; }
         [Inject] private HttpClient Client { get; set; }
 
         [Inject] private EpochAuthProvider Auth { get; set; }
@@ -26,8 +33,42 @@ namespace EpochApp.Client.Pages.Dashboard.Worlds
             await base.OnInitializedAsync();
 
             var worlds = await Client.GetFromJsonAsync<List<WorldDTO>>($"api/v1/Worlds/User?ownerId={Auth.CurrentUser.UserID}");
+            var newWorlds = await Client.GetFromJsonAsync<List<UserWorldDTO>>($"api/v2/Worlds/UserWorlds?userId={Auth.CurrentUser.UserID}");
+            if (newWorlds.Any())
+                _newUserWorlds.AddRange(newWorlds);
             if (worlds.Any())
                 _userWorlds.AddRange(worlds);
+        }
+
+        private async Task HandleWorldDeletionAsync(UserWorldDTO world)
+        {
+            if (_userWorlds.Count < 2)
+            {
+                var messageResult = await DialogService.ShowMessageBox(
+                                    "Warning - Cannot Delete Only World!",
+                                    $"You cannot delete the world of {world.WorldName}. This is your only world! You must create a new world before you can delete this one.",
+                                    "Okay", cancelText: "Cancel");
+                return;
+            }
+
+            var result = await DialogService.ShowMessageBox(
+                         "Warning - Deleting World!",
+                         $"You are about to delete the world of {world.WorldName}. This action cannot be undone! Are you sure you want to proceed?",
+                         "Yes! Delete World!", cancelText: "Wait! I've changed my mind!");
+            if (result != true)
+                return;
+
+            var response = await Client.DeleteFromJsonAsync<WorldDTO>($"api/v1/Worlds/{Auth.CurrentUser.UserID}/{world.WorldId}");
+            if (response != null)
+            {
+                if (_userWorlds.Any(x => x.WorldID == response.WorldID))
+                {
+                    var worldToDelete = _newUserWorlds.FirstOrDefault(x => x.WorldId == response.WorldID);
+                    _userWorlds.Remove(response);
+                    _newUserWorlds.Remove(worldToDelete);
+                    StateHasChanged();
+                }
+            }
         }
 
         private async Task HandleWorldDeletionAsync(WorldDTO world)
